c961642583b9b203145909ca7f030b4fd38cd8b2
Simone Carcagnì <simonecarcagni@libero.it>
2023-05-24 18:01:52 +0200

    Creazione di Model, Seeder e migration effettuata

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['Mobile App', 'Gestionale', 'Sito Internet'];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($type, '-');
+            $newType->save();
+        };
+    }
+}
